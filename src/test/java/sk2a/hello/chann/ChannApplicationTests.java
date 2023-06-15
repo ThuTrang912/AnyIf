695d74787a4c596f59ab011a5c8da1776ce73404
Nahc219 <thutrang2220047@gmail.com>
2023-06-15 11:56:00 +0900

    hello spring

---
+++ src/test/java/sk2a/hello/chann/ChannApplicationTests.java
@@ -0,0 +1,13 @@
+package sk2a.hello.chann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChannApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
