5e7b8387e0e75cffd40380e5a4a78950b821d3b0
Nahc219 <thutrang2220047@gmail.com>
2023-07-02 17:49:10 +0900

    login And Home

---
+++ src/main/resources/static/js/Timer.js
@@ -0,0 +1,29 @@
+{
+  const u_text = document.querySelector("#user");
+  const p_text = document.querySelector("#post");
+  const ok_text = document.querySelector("#ok");
+  const user_target = 2310;
+  const post_target = 1320;
+  const ok_target = 593;
+  let a = 0;
+  let b = 0;
+  let c = 0;
+
+  let inter = setInterval(() => {
+    u_text.innerText = a;
+    if (a == user_target) clearInterval(inter);
+    a++;
+  }, 3);
+
+  let inter2 = setInterval(() => {
+    p_text.innerText = b;
+    if (b == post_target) clearInterval(inter2);
+    b++;
+  }, 5);
+
+  let inter3 = setInterval(() => {
+    ok_text.innerText = c;
+    if (c == ok_target) clearInterval(inter3);
+    c++;
+  }, 10);
+}
